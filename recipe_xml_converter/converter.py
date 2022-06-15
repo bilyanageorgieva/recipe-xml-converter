5f232a1e16cd4c35b2a52a720eae93efbbcbda8a
bilyanageorgieva <bibby.georgieva@gmail.com>
2022-06-15 08:45:55 +0200

    refactor: split code into functions

--- recipe_xml_converter/converter.py
+++ recipe_xml_converter/converter.py
@@ -1,18 +1,27 @@
 from pathlib import Path
+from typing import Union
 
 from lxml import etree as ET
 
 
-def main():
-    dom = ET.parse(Path(__file__).parent.parent / "data/'9os_Style_Chicken_Salad.xml")
+def convert_recipe(dom: ET._ElementTree) -> ET._XSLTResultTree:
+    # TODO: fix types
     xslt = ET.parse(Path(__file__).parent.parent / "data/recipe.xsl")
     transform = ET.XSLT(xslt)
     new_dom = transform(dom)
-    print(ET.tostring(new_dom, pretty_print=True))
 
     with open(Path(__file__).parent.parent / "data/tranformed.xml", "wb") as file:
         file.write(ET.tostring(new_dom, pretty_print=True))
 
+    return new_dom
 
-if __name__ == '__main__':
-    main()
+
+def convert_recipe_from_file(path: Union[Path, str]) -> ET._ElementTree:
+    dom = ET.parse(path)
+    return convert_recipe(dom)
+
+
+if __name__ == "__main__":
+    convert_recipe_from_file(
+        Path(__file__).parent.parent / "data/'9os_Style_Chicken_Salad.xml"
+    )
